71be37a34c96017c32a810cedba7907312555626
Jeff <jeffgichuki68@gmail.com>
2023-04-08 11:22:13 +0300

    added notif close button

--- src/components/notification/notification.js
+++ src/components/notification/notification.js
@@ -1,23 +1,32 @@
 import React, { useState } from "react";
-import './notification.css'
+import "./notification.css";
 
 function Notification() {
+  const [notifState, setNotifState] = useState("active");
 
+  let toggleNotification = ()=>{
+    if(notifState === "active"){
+        setNotifState("inactive");
+    }
+    else{
+        setNotifState("active");
+    }
+  };
 
-    return ( 
-        <div className="notif-active notif-success" id="notification-container">
-
-            <div className="notif-section alert-section"></div>
-            <div className="notif-section message-section">
-                <h4>Login successful </h4>
-            </div>
-            <div className="notif-section btn-section">
-                <i className="material-icons">close</i>
-            </div>
-
-
-        </div>
-     );
+  return (
+    <div
+      className={`notif-${notifState} notif-success`}
+      id="notification-container"
+    >
+      <div className="notif-section alert-section"></div>
+      <div className="notif-section message-section">
+        <h4>Login successful </h4>
+      </div>
+      <div className="notif-section btn-section">
+        <i onClick={()=>{toggleNotification()}} className="material-icons">close</i>
+      </div>
+    </div>
+  );
 }
 
 export default Notification;
