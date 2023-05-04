ddec0dec8ea77b2d49873d14ba215e56ee08e693
Jeff <jeffgichuki68@gmail.com>
2023-05-04 11:53:19 +0300

    added

---
+++ src/components/mainbody/mainbody.js
@@ -0,0 +1,11 @@
+import "./mainbody.css";
+function MainBody() {
+  return (
+    <section id="main-body">
+      <div className="main-body-col stats-col"></div>
+      <div className="main-body-col books-col"></div>
+    </section>
+  );
+}
+
+export default MainBody;
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -1,15 +1,15 @@
 import BookSlider from "../../components/Bookslider/Bookslider";
+import MainBody from "../../components/mainbody/mainbody";
 import Navbar from "../../components/navbar/navbar";
-import './homepage.css'
+import "./homepage.css";
 function Homepage() {
-    return ( 
-        <div id="homepage-container">
-            <Navbar/>
-            <div>
-                <BookSlider/>
-            </div>
-        </div>
-     );
+  return (
+    <div id="homepage-container">
+      <Navbar/>
+      <BookSlider/>
+      <MainBody/>
+    </div>
+  );
 }
 
 export default Homepage;
